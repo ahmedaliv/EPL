d518507a4cdba5f1885bcf89bbf1464976d85a93
sekovv <66169978+ahmed1958@users.noreply.github.com>
2023-05-22 15:38:15 +0300

    Gui  added

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from globals import *
 from team import Team
 from date import Date
 from utils import *
-import numpy as np
-import pandas as pd
+from support import *
 def add_teams(team1,team2):
     if team1 not in teams:
         teams[team1]=Team()
@@ -44,25 +43,6 @@ def edit_standing(filter_method,limit):
             if(date.date<=limit):
                 date.traverse_matches()    
 
-def export_standings_csv(filename, filter_type=None, filter_value=None):
-    if filter_type and filter_value:
-        filename = f"standings_{filter_type}_{filter_value}.csv"
-
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Team', 'MatchPlayed', 'W', 'D', 'L', 'Goals For', 'Goals Against', 'Goal Diff', 'Points'])
-
-        for team in teams:
-            writer.writerow([team,
-                             teams[team].matches_played,
-                             teams[team].matches_won,
-                             teams[team].matches_drawn,
-                             teams[team].matches_lost,
-                             teams[team].for_goals,
-                             teams[team].against_goals,
-                             teams[team].net_goals,
-                             teams[team].points])
-
 def filter_by_round():
     round_number = int(input("Enter the round number: "))
     edit_standing('round', round_number)
@@ -97,15 +77,9 @@ if __name__ == '__main__':
             print("Invalid Input")
             if not try_again():
                 exit()
-
         sorted_teams = sorted(teams.items(), key=lambda x: x[1].points, reverse=True)
         teams = dict(sorted_teams)
-        export_standings_csv('standings.csv', filter_type, filter_value)
-        print(f"Standings exported to standings_{filter_type}_{filter_value}.csv")
-        std=pd.read_csv(f'standings_{filter_type}_{filter_value}.csv')
-        std.index=np.arange(1, len(std) + 1)
-        print(std)
-
+        draw(teams)
         if try_again():
             continue
         else:
